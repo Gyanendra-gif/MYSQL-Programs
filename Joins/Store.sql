5ff33ae58128fb4530e167c6703eb6211611156e
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-02-02 16:24:02 +0530

    [Gyanendra] Add: A Program of Basic Joins is Added

--- Joins/Store.sql
+++ Joins/Store.sql
@@ -6,9 +6,13 @@ insert into items (product_name,price,quantity) values ('HP', 30000.00, 2);
 insert into items (product_name,price,quantity) values ('DELL', 40000.00, 3);
 insert into items (product_name,price,quantity) values ('LENOVO', 50000.00, 4);
 insert into items (product_name,price,quantity) values ('SONY', 60000.00, 5);
+insert into items (product_name,price,quantity) values ('SAMSUNG', 70000.00, 6);
 
 create table customer(cid int ,foreign key(cid)references items(id), name varchar(50), city varchar(50));
 insert into customer values (1, 'Sam','Mumbai');
 insert into customer values (2, 'Tom','Delhi');
 insert into customer values (3, 'Harry','Banglore');
 insert into customer values (4, 'Sam','Pune');
+insert into customer value (5, 'Max', 'Mumbai');
+
+select customer.cid, customer.name from customer cross join items;
